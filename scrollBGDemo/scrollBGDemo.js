bca04ebcd5ed932c333f587cbc8fe1de7fb80335
Jonah Chazan <jechazan@gmail.com>
2017-01-01 13:16:22 -0500

    better curve to animation

--- scrollBGDemo/scrollBGDemo.js
+++ scrollBGDemo/scrollBGDemo.js
@@ -14,9 +14,11 @@ function setup() {
 function draw() {
   background(0);
   var t = $("#holder").scrollTop() / ($("#main-content").height() - $("#holder").height());
-  tint(255, 255, 255, abs((1 - t * 2)) * 255);
-  image(dayImg, 0, 0);
+  tint(255, 255, 255, (cos(t * 2 * PI) + 1) / 2 * 255);
+  image(dayImg, 0, 0, max(width, height / dayImg.height * dayImg.width), max(height, width / dayImg.width * dayImg.height));
+  console.log(t * 2 * PI);
   
-  tint(255, 255, 255, (1 - abs((1 - t * 2))) * 255);
-  image(nightImg, 0, 0);
+  tint(255, 255, 255, (-cos(t * 2 * PI) + 1) / 2 * 255);
+  
+  image(nightImg, 0, 0, max(width, height / nightImg.height * nightImg.width), max(height, width / nightImg.width * nightImg.height));
 }
